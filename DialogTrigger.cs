562b34f51547f101e50c9cb8ff4bae7a98fc8054
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-06-04 08:52:41 -0400

    Scripts

---
+++ DialogTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+	bool triggered;
+	
+	public void TriggerDialogue() {
+		if (!triggered) {
+			FindObjectOfType<DialogManager>().StartDialogue(dialogue);
+			triggered = true;
+		} else
+			FindObjectOfType<DialogManager>().DisplayNextSentence();
+	}
+}
---
+++ Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Dialogue
+{	
+	public AudioClip[] sounds;
+	public Sprite[] characters;
+	public string[] names;
+	[TextArea(3, 10)]
+    public string[] sentences;
+}
